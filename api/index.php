29b014c47d6fb2bbccdf46049598ed2a9399563b
Simbah_Ngoding <gilangresma02@gmail.com>
2025-07-01 17:02:57 +0700

    Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
-<?php  
+<?php
 
-require __DIR__ . "/../public/index.php";
+// Set proper server variables
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'] ?? '/';
+
+// Forward to Laravel
+require __DIR__ . '/../public/index.php';

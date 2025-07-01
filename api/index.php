6a546539bcf70ca5bb8b728ad8941e0263a3ed0f
Simbah_Ngoding <gilangresma02@gmail.com>
2025-07-01 17:08:14 +0700

    index

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
 
+// Set writable paths for Vercel
+if (isset($_ENV['VERCEL'])) {
+    // Create temp directories
+    $tempDirs = ['/tmp/views', '/tmp/cache', '/tmp/sessions'];
+    foreach ($tempDirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+    }
+}
+
 // Set proper server variables
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
